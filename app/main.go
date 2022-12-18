e1e8fe91161311e136cbbbe35022df03578b72f4
tyage <namatyage@gmail.com>
2022-12-18 19:12:35 +0900

    note editor

--- app/main.go
+++ app/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"html/template"
 	"net/http"
 	"os"
@@ -10,42 +10,102 @@ import (
 	"github.com/google/uuid"
 )
 
+type NoteParam struct {
+	Id   uuid.UUID
+	Body string
+}
+
+const (
+	PublicDir    = "./public"
+	NoteBaseDir  = "./notes"
+	NoteTemplate = "./templates/note.html"
+)
+
+func WriteNote(file *os.File, body NoteParam) {
+	tmpl, err := template.ParseFiles(NoteTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	err = tmpl.Execute(file, body)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetNotePath(noteId uuid.UUID) (string, string) {
+	noteFile := noteId.String() + ".html"
+	notePublicPath := filepath.Join(NoteBaseDir, noteFile)
+	noteFilePath := filepath.Join(PublicDir, notePublicPath)
+
+	return noteFilePath, notePublicPath
+}
+
+func ValidateNoteMemo(memo string) error {
+	if len(memo) > 1024 {
+		return errors.New("memo is too long")
+	}
+	return nil
+}
+
 func main() {
-	publicDir := "./public"
-	siteBaseDir := "./sites"
 
-	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
-		siteId := uuid.New().String()
-		newSiteDir := filepath.Join(publicDir, siteBaseDir, siteId)
+	http.HandleFunc("/new-note", func(w http.ResponseWriter, r *http.Request) {
+		memo := r.FormValue("memo")
 
-		err := os.Mkdir(newSiteDir, 0755)
+		// validate memo
+		err := ValidateNoteMemo(memo)
 		if err != nil {
-			panic(err)
+			http.Error(w, "invalid memo", http.StatusInternalServerError)
+			return
 		}
 
-		http.Redirect(w, r, filepath.Join(siteBaseDir, siteId), http.StatusFound)
+		noteId := uuid.New()
+		noteFilePath, noteURL := GetNotePath(noteId)
+
+		f, err := os.Create(noteFilePath)
+		if err != nil {
+			http.Error(w, "failed memo creation", http.StatusInternalServerError)
+			return
+		}
+
+		WriteNote(f, NoteParam{Id: noteId, Body: memo})
+
+		http.Redirect(w, r, noteURL, http.StatusFound)
 	})
 
-	http.HandleFunc("/save", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.New("test").Parse("<p>{{.}}</p>")
+	http.HandleFunc("/update-note", func(w http.ResponseWriter, r *http.Request) {
+		noteId := r.FormValue("noteId")
+		memo := r.FormValue("memo")
+
+		// validate noteId
+		uuidNoteId, err := uuid.Parse(noteId)
 		if err != nil {
-			panic(err)
+			http.Error(w, "invalid noteId", http.StatusInternalServerError)
+			return
 		}
 
-		f, err := os.Create("/tmp/memo.html")
+		// validate memo
+		err = ValidateNoteMemo(memo)
 		if err != nil {
-			panic(err)
+			http.Error(w, "invalid memo", http.StatusInternalServerError)
+			return
 		}
 
-		err = tmpl.Execute(f, r.FormValue("memo"))
+		noteFilePath, noteURL := GetNotePath(uuidNoteId)
+
+		f, err := os.Open(noteFilePath)
 		if err != nil {
-			panic(err)
+			http.Error(w, "invalid note", http.StatusInternalServerError)
+			return
 		}
 
-		fmt.Fprintf(w, "done")
+		WriteNote(f, NoteParam{Id: uuidNoteId, Body: memo})
+
+		http.Redirect(w, r, noteURL, http.StatusFound)
 	})
 
-	http.Handle("/", http.FileServer(http.Dir(publicDir)))
+	http.Handle("/", http.FileServer(http.Dir(PublicDir)))
 
 	http.ListenAndServe(":3000", nil)
 }
