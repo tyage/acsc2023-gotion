971d8eda131fb5786f4854faa5668e23fd35b673
tyage <namatyage@gmail.com>
2022-12-18 16:31:10 +0900

    create site

--- app/main.go
+++ app/main.go
@@ -5,9 +5,27 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"path/filepath"
+
+	"github.com/google/uuid"
 )
 
 func main() {
+	publicDir := "./public"
+	siteBaseDir := "./sites"
+
+	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
+		siteId := uuid.New().String()
+		newSiteDir := filepath.Join(publicDir, siteBaseDir, siteId)
+
+		err := os.Mkdir(newSiteDir, 0755)
+		if err != nil {
+			panic(err)
+		}
+
+		http.Redirect(w, r, filepath.Join(siteBaseDir, siteId), http.StatusFound)
+	})
+
 	http.HandleFunc("/save", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.New("test").Parse("<p>{{.}}</p>")
 		if err != nil {
@@ -26,6 +44,8 @@ func main() {
 
 		fmt.Fprintf(w, "done")
 	})
-	http.Handle("/", http.FileServer(http.Dir("/tmp/")))
+
+	http.Handle("/", http.FileServer(http.Dir(publicDir)))
+
 	http.ListenAndServe(":3000", nil)
 }
