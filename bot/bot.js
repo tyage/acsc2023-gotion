0e4e8a768e542389a123daaec88be49922b8b4d0
tyage <namatyage@gmail.com>
2023-01-09 14:11:19 +0900

    queue bots

--- bot/bot.js
+++ bot/bot.js
@@ -4,53 +4,96 @@ const net = require('net')
 const APP_BASEURL = process.env.APP_BASEURL || 'http://nginx/'
 const APP_HOST = process.env.APP_HOST || 'nginx'
 const FLAG = process.env.FLAG || 'CTF{dummyflag}'
+const BROWSER_POOL = process.env.BROWSER_POOL || 4
 
 const sleep = d => new Promise(r => setTimeout(r, d));
 
-const crawl = async (path = '') => {
-  const url = APP_BASEURL + path
+class CralwQueue {
+  browsers = []
+  isBrowserAvailable = new Map()
+  queue = []
 
-  console.log("crawl start:", url)
+  constructor(browserPoolSize) {
+    // launch browsers
+    for (let i = 0; i < browserPoolSize; ++i) {
+      this.launchBrowser()
+    }
 
-  // TODO: launch browsers on initialization and reuse like this
-  // https://github.com/google/google-ctf/blob/85f182eac4472f58fb1ab3b708384331a1f4d3ca/2022/quals/web-postviewer-bot/bot.js#L96
-  const browser = await pptr.launch({
-    args: ["--no-sandbox"]
-  })
-
-  const context = await browser.createIncognitoBrowserContext()
-
-  // create page
-  const page = await context.newPage()
-
-  await page.setCookie({
-    name: "FLAG",
-    value: FLAG,
-    domain: APP_HOST,
-    path: "/",
-  });
-
-  try {
-    await page.goto(url, {
-      timeout: 2000,
-    });
-    await sleep(2000);
-  } catch (e) {
-    console.log('error', e);
+    this.loop()
   }
 
-  await page.close();
-  await context.close();
-  await browser.close();
+  add(path) {
+    this.queue.push(path)
+  }
 
-  console.log("crawl end:", url)
+  async launchBrowser() {
+    const browser = await pptr.launch({
+      args: ["--no-sandbox"]
+    })
+    this.browsers.push(browser)
+    this.isBrowserAvailable.set(browser, true)
+  }
+
+  async loop() {
+    this.browsers.forEach((browser) => {
+      if (!this.isBrowserAvailable.get(browser)) {
+        return
+      }
+      if (this.queue.length === 0) {
+        return
+      }
+
+      const queue = this.queue.shift()
+      this.isBrowserAvailable.set(browser, false)
+      console.log(`occupy browser: ${[...this.isBrowserAvailable.values()]}`)
+      this.crawl(browser, queue).finally(() => {
+        this.isBrowserAvailable.set(browser, true)
+      })
+    })
+
+    await sleep(100)
+    this.loop()
+  }
+
+  async crawl(browser, path = '') {
+    const url = APP_BASEURL + path
+
+    console.log("crawl start:", url)
+
+    const context = await browser.createIncognitoBrowserContext()
+
+    // create page
+    const page = await context.newPage()
+
+    await page.setCookie({
+      name: "FLAG",
+      value: FLAG,
+      domain: APP_HOST,
+      path: "/",
+    });
+
+    try {
+      await page.goto(url, {
+        timeout: 2000,
+      });
+      await sleep(2000);
+    } catch (e) {
+      console.log('error', e);
+    }
+
+    await page.close();
+    await context.close();
+
+    console.log("crawl end:", url)
+  }
 }
 
 const main = async () => {
+  const queue = new CralwQueue(BROWSER_POOL)
   const server = net.createServer((socket) => {
     socket.on('data', (data) => {
       const path = data.toString().trim()
-      crawl(path)
+      queue.add(path)
     })
   })
   server.listen(3000, '0.0.0.0')
